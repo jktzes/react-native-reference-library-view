7d2777b9b28b9347945d835e23ad0f4c9c3cd5ce
Ezo Tang <jktzes@gmail.com>
2020-03-11 09:45:04 +0800

    modified the view controller that handles the render of dictionary UI

--- ios/ReferenceLibraryView.m
+++ ios/ReferenceLibraryView.m
@@ -18,8 +18,9 @@ RCT_EXPORT_METHOD(showDefinitionForTerm:(NSString*)term callback:(RCTResponseSen
 
     dispatch_async(dispatch_get_main_queue(), ^{
       UIReferenceLibraryViewController *referenceLibraryVC = [[UIReferenceLibraryViewController alloc] initWithTerm:term];
-      UIViewController *rootVC = ((AppDele*)[UIApplication sharedApplication].delegate).window.rootViewController;
-      [rootVC presentViewController:referenceLibraryVC animated:YES completion:nil];
+      // UIViewController *rootVC = ((AppDele*)[UIApplication sharedApplication].delegate).window.rootViewController;
+      UIViewController *ctrl = RCTPresentedViewController();
+      [ctrl presentViewController:referenceLibraryVC animated:YES completion:nil];
     });
   }
 
