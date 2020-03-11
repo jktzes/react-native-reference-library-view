8979fc7354f3352fe9de6bc145ee85f30021a4e7
Ezo Tang <jktzes@gmail.com>
2020-03-11 10:08:53 +0800

    show dictionary regardless of definition found or not

--- ios/ReferenceLibraryView.m
+++ ios/ReferenceLibraryView.m
@@ -21,13 +21,14 @@ RCT_EXPORT_METHOD(showDefinitionForTerm:(NSString*)term callback:(RCTResponseSen
   {
     hasDefinition = YES;
 
-    dispatch_async(dispatch_get_main_queue(), ^{
-      UIReferenceLibraryViewController *referenceLibraryVC = [[UIReferenceLibraryViewController alloc] initWithTerm:term];
-      // UIViewController *rootVC = ((AppDele*)[UIApplication sharedApplication].delegate).window.rootViewController;
-      UIViewController *ctrl = RCTPresentedViewController();
-      [ctrl presentViewController:referenceLibraryVC animated:YES completion:nil];
-    });
   }
+
+  dispatch_async(dispatch_get_main_queue(), ^{
+    UIReferenceLibraryViewController *referenceLibraryVC = [[UIReferenceLibraryViewController alloc] initWithTerm:term];
+    // UIViewController *rootVC = ((AppDele*)[UIApplication sharedApplication].delegate).window.rootViewController;
+    UIViewController *ctrl = RCTPresentedViewController();
+    [ctrl presentViewController:referenceLibraryVC animated:YES completion:nil];
+  });
 
   callback(@[@(hasDefinition)]);
 }
